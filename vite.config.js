62b55027c178f6e3f1de6b15a71d7caaa7b8390e
BangZaky0029 <bangzaky0029@gmail.com>
2026-01-13 13:47:23 +0700

    Update vite base dan perubahan terbaru

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/surat-perintah-kerja/',
+  base: '/generator-surat/surat-perintah-kerja/',
 })
